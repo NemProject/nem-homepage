531fe33ad4e391cc74656c246471bcf2b84b053b
crackerhead <nbower@mac.com>
2015-04-21 15:14:40 -0700

    warn IE users, start stats implementation

--- js/_custom.js
+++ js/_custom.js
@@ -34,7 +34,7 @@ $.i18n.init({
 		$('#statsnis').text($.t('stats.nis'));
 		$('#statsacc').text($.t('stats.acc'));
 		$('#statsdorm').text($.t('stats.dorm'));
-		$('#statsavg').text($.t('stats.avg'));
+		$('#statsVer').text($.t('stats.ver'));
 
 		$('#buyheader').text($.t('buy.header'));
 		$('#buybox1').text($.t('buy.box1'));
@@ -302,3 +302,26 @@ $(function() {
     }
   });
 });
+
+//**## IE DETECTION AND REDIRECT ##**//
+function GetIEVersion() {
+	var sAgent = window.navigator.userAgent;
+	var Idx = sAgent.indexOf("MSIE");
+	// If IE, return version number.
+		if (Idx > 0)
+			return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+		// If IE 11 then look for Updated user agent string.
+		else if (!!navigator.userAgent.match(/Trident\/7\./))
+			return 11;
+		else
+			return 0; //It is not IE
+		}
+		if (GetIEVersion() > 0)
+		var txt="It looks like you are using Internet Explorer, for the best experience, you should view this site in ";
+		var or=" or ";
+		var firefox="<a href=\"https://www.mozilla.org/en-US/firefox/new/\">Firefox</a>";
+		var chrome="<a href=\"http://www.google.com/chrome/\">Chrome</a>";
+		var cancel = "<i class=\"icon-cancel-circled\" onclick=\"parentNode.remove()\"></i>";	$('#headerIE').html(txt+chrome+or+firefox+cancel);
+
+		if (GetIEVersion() == 0)
+		$('#headerIE').remove();
